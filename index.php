1e472f1b6f2dc2d95edccef5df7ed7e2ef575589
Beveik <85123753+Beveik@users.noreply.github.com>
2021-08-02 12:30:50 +0300

    patobulinimas
    Pridėjau daugiau išvestų rezultatų

--- index.php
+++ index.php
@@ -16,12 +16,14 @@
 
 
 <?php
-// 1. Sukurkite funkciją, kurią iškvietus, masyvą galima papildyti norimu elementu. Masyvas išsaugomas į COOKIE.
+// 1. Sukurkite funkciją, kurią iškvietus, masyvą galima papildyti norimu elementu. 
+// Masyvas išsaugomas į COOKIE.
 // Informacija paimama iš input laukelio. Funkcija iškviečiama paspaudus mygtuką.
 
 function skaiciavimoFunkcija(){
     if(isset($_GET["elementas"]) && !empty($_GET["elementas"])) {
 $elementas=$_GET["elementas"];
+$rezultatas=$elementas;
 
 if (isset($_COOKIE["elementas"])) {
 $elementuMasyvas=explode("|", $_COOKIE["elementas"]);
@@ -35,7 +37,13 @@ array_push($elementuMasyvas, $elementas);
 //implode - masyva vercia i teksta
 setcookie("elementas", implode("|", $elementuMasyvas), time()+3600, "/");
 
+    } else {
+        echo $_COOKIE["elementas"];
+        $rezultatas= "Laukelis tuščias.";
     }
+
+    echo "<br>Jūs pridėjote:<br>";
+    echo $rezultatas;
 //     $duomenuMasyvas = 0;
 //    for ($i=0; $i<$duomenuMasyvas.length; $i++){
 //     $duomenuMasyvas = explode($simbolis, $elementas);
